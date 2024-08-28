3e475442ba07e25d7c5bc7e59c558d33afee37d3
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-08-28 06:42:38 +0300

    mongodb init

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
+
+export const initMongoConnection = async () => {
+    try {
+        const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+        await mongoose.connect(connectionString);
+        console.log('Mongo connection successfully established!');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
+};
