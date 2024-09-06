5d91ed01869652a77cf355594301cdb051b8edc4
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-06 22:34:02 +0300

    added services

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import {Contact} from "../db/models/contacts.js"
+import {Contact} from "../db/models/contacts.js";
 
 export const getContacts = async () => {
         const contacts = await Contact.find();
@@ -8,4 +8,19 @@ export const getContacts = async () => {
 export const getContactById = async (contactId) => {
         const contact = await Contact.findById(contactId);
         return contact;
+};
+
+export const createContact = async(payload) =>{
+        const contact = await Contact.create(payload); 
+        return contact;
+};
+
+export const updateContact = async(id, payload) =>{
+        const contact = await Contact.findByIdAndUpdate(id, payload, {new: true});
+        return contact;
+};
+
+export const deleteContact = async(id) =>{
+       const contact = await Contact.findByIdAndDelete(id);
+       return contact;
 };
