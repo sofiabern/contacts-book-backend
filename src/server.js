c6662be8ebbb115534f98bf7d50c602ed9873ce9
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-06 22:33:14 +0300

    updated server.js

--- src/server.js
+++ src/server.js
@@ -4,29 +4,31 @@ import pino from 'pino';
 import pinoHttp from 'pino-http';
 import dotenv from 'dotenv';
 
-import { getAllContacts, getContactByIdController } from './controllers/contacts.js';
+import { router } from './routers/contacts.js  ';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 dotenv.config();
 
 export const setupServer = () => {
   const app = express();
 
+  app.use(express.json());
+
   app.use(cors());
 
   const logger = pino();
   app.use(pinoHttp({ logger }));
 
-  app.get('/contacts', getAllContacts);
+  app.use(router);
 
-  app.get('/contacts/:contactId', getContactByIdController);
+  app.use(notFoundHandler);
 
-  app.use((req, res, next) => {
-    res.status(404).json({ message: 'Not found' });
-});
+  app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
 };
