aab994fe49e287c0dc27334ead36b57f678a73a3
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-17 03:29:01 +0300

    updated errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,20 +1,25 @@
 import { isHttpError } from 'http-errors';
 import { MongooseError } from 'mongoose';
 
+
 export const errorHandler = (error, req, res, next) => {
+
+
   if (isHttpError(error)) {
     return res.status(error.status).json({
       status: error.status,
       message: error.message,
-      ...(error.errors ? { errors: error.errors } : {}),
+      ...(error.errors ? { errors: error.errors.map(err => err.message.replace(/"/g, "'"))} : {}),
     });
   }
+
+
 
   if (error instanceof MongooseError) {
     return res.status(500).json({
       status: 500,
       message: 'Mongoose error',
-      data: error.message,
+      data: error.message.replace(/(?<!\.\s)\./g, ''),
     });
   }
 
