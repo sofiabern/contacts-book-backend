d4867e91e37ad6d6c7a726d6a19c5b2d1dc760de
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-17 03:31:31 +0300

    implemented pagination, sort and filter

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,26 +1,64 @@
-import {Contact} from "../db/models/contacts.js";
+import { Contact } from '../db/models/contacts.js';
 
-export const getContacts = async () => {
-        const contacts = await Contact.find();
-        return contacts;
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+
+export const getContacts = async ({
+  page,
+  perPage,
+  sortOrder,
+  sortBy,
+  filter,
+}) => {
+        
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = Contact.find();
+
+  console.log(filter);
+
+  if (filter.type) {
+    contactsQuery.where('contactType').equals(filter.type);
+  }
+
+  if (filter.isFavourite !== undefined) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+
+  const contactsCount = await Contact.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
-        const contact = await Contact.findById(contactId);
-        return contact;
+  const contact = await Contact.findById(contactId);
+  return contact;
 };
 
-export const createContact = async(payload) =>{
-        const contact = await Contact.create(payload); 
-        return contact;
+export const createContact = async (payload) => {
+  const contact = await Contact.create(payload);
+  return contact;
 };
 
-export const updateContact = async(id, payload) =>{
-        const contact = await Contact.findByIdAndUpdate(id, payload, {new: true});
-        return contact;
+export const updateContact = async (id, payload) => {
+  const contact = await Contact.findByIdAndUpdate(id, payload, { new: true });
+  return contact;
 };
 
-export const deleteContact = async(id) =>{
-       const contact = await Contact.findByIdAndDelete(id);
-       return contact;
+export const deleteContact = async (id) => {
+  const contact = await Contact.findByIdAndDelete(id);
+  return contact;
 };
