f80751f044a1bd49229de2482ae3e2d817410acc
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-17 03:29:44 +0300

    added id validation

---
+++ src/middlewares/isValid.js
@@ -0,0 +1,17 @@
+import { isValidObjectId } from 'mongoose';
+import createHttpError from 'http-errors';
+
+export const isValidId = (idName) => (req, res, next) => {
+    const id = req.params[idName];
+
+    if (!id) {
+        return next(createHttpError(400, 'Id should be specified'));
+    }
+
+    if (!isValidObjectId(id)) {
+      return next(createHttpError(400, 'Bad Request'));
+    }
+
+    return next();
+  };
+
