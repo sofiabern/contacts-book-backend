3368ee0c11979876025323d25736e5d5cdd17e3c
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-17 03:31:01 +0300

    implemented validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,13 +2,20 @@ import express from "express";
 
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
+import { validateBody } from "../middlewares/validateBody.js";
+
+import { isValidId } from "../middlewares/isValid.js";
+
+import { createContactSchema } from "../validation/contacts.js";
+import { updateContactSchema } from "../validation/contacts.js";
+
 import { createContactController, deleteContactController, getAllContactsController,  getContactByIdController, updateContactController } from "../controllers/contacts.js";
 
 export const router = express.Router();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(updateContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.get('/contacts/:contactId', isValidId('contactId'), ctrlWrapper(getContactByIdController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.patch('/contacts/:contactId', isValidId('contactId'), validateBody(updateContactSchema), ctrlWrapper(updateContactController));
+router.delete('/contacts/:contactId', isValidId('contactId'), ctrlWrapper(deleteContactController));
 
