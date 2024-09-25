8d651e07491cbe15ebcfadb343cdd58eb82b6a98
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-25 13:07:12 +0300

    created/created joi schemas

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -3,7 +3,11 @@ import Joi from "joi";
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string().min(3).max(20).required(),
-    email: Joi.string().email().optional(),
+    email: Joi.string()
+    .pattern(/^\S+@\S+\.\S+$/, 'email')
+    .messages({
+      'string.pattern.name': 'Invalid email format'
+    }).optional(),
     isFavourite: Joi.boolean().optional(),
     contactType: Joi.string().valid('work', 'home', 'personal').optional()
 });
@@ -11,7 +15,11 @@ export const createContactSchema = Joi.object({
 export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).optional(),
     phoneNumber: Joi.string().min(3).max(20).optional(),
-    email: Joi.string().email().optional(),
+    email: Joi.string()
+    .pattern(/^\S+@\S+\.\S+$/, 'email')
+    .messages({
+      'string.pattern.name': 'Invalid email format'
+    }).optional(),
     isFavourite: Joi.boolean().optional(),
     contactType: Joi.string().valid('work', 'home', 'personal').optional()
 });
