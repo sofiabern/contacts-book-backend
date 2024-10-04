72dcacfeaf5a99e056662927ea6a39720ae2b88c
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-10-04 19:26:57 +0300

    updated constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,17 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
 export const SORT_ORDER = {
-    ASC: 'asc',
-    DESC: 'desc',
-  };
-  
+  ASC: 'asc',
+  DESC: 'desc',
+};
+
+export const SMTP = {
+  HOST: process.env.SMTP_HOST,
+  PORT: Number(process.env.SMTP_PORT),
+  USER: process.env.SMTP_USER,
+  PASSWORD: process.env.SMTP_PASSWORD,
+  FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
+};
+
+console.log(SMTP);
