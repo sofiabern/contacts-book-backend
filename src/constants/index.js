cf8684caa9c4a32913f7e5a0f007df992c3f7c96
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-10-06 22:06:38 +0300

    updated env and const

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,4 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-
+import path from "node:path";
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
@@ -8,10 +6,10 @@ export const SORT_ORDER = {
 
 export const SMTP = {
   HOST: process.env.SMTP_HOST,
-  PORT: Number(process.env.SMTP_PORT),
+  PORT: process.env.SMTP_PORT,
   USER: process.env.SMTP_USER,
   PASSWORD: process.env.SMTP_PASSWORD,
   FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
 };
 
-console.log(SMTP);
+export const TEMPLATE_DIR = path.resolve("src","templates");
