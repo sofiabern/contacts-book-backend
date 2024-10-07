7ba173eb3c6e8aafa3b92a0a0dda75244dff13a4
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-10-07 09:07:21 +0800

    upgraded

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { setupServer } from "./server.js";
 import { initMongoConnection } from "./db/initMongoConnection.js";
-
+import { createFolderIfNotExist } from "./utils/createFolderIfNotExist.js";
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from "./constants/index.js";
 (async () => {
     await initMongoConnection();
+    await createFolderIfNotExist(TEMP_UPLOAD_DIR);
+    await createFolderIfNotExist(UPLOAD_DIR);
     setupServer();
 })();
--- src/server.js
+++ src/server.js
@@ -8,11 +8,14 @@ import cookiesParser from 'cookie-parser';
 import { router } from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { UPLOAD_DIR } from './constants/index.js';
 
 export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.use(cors());
 
