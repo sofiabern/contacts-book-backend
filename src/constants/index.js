6b1f60693edea4ccbedd1dd3ad659894262d5d11
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-10-07 09:06:38 +0800

    added new const

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,7 +9,13 @@ export const SMTP = {
   PORT: process.env.SMTP_PORT,
   USER: process.env.SMTP_USER,
   PASSWORD: process.env.SMTP_PASSWORD,
-  FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
+  FROM: process.env.SMTP_FROM,
 };
 
+export const APP_DOMAIN = process.env.APP_DOMAIN;
+
 export const TEMPLATE_DIR = path.resolve("src","templates");
+export const TEMP_UPLOAD_DIR = path.resolve("temp")
+export const UPLOAD_DIR = path.resolve( 'upload');
+
+export const BACKEND_HOST = process.env.BACKEND_HOST;
