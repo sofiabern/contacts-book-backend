be9db322e40475d65788327e7ad153f6e9bafb37
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-10-13 14:20:11 +0300

    improved

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -41,7 +41,7 @@ export const getContactByIdController = async (req, res, next) => {
     return next(createHttpError(404, 'Contact not found'));
   }
 
-  res.status(200).json({ status: 200, data: contact });
+  res.status(200).json({ status: 200, message: `Successfully found a contact with id ${contactId}`,  data: contact });
 };
 
 export const createContactController = async (req, res) => {
