246c7b81d8747acf61e6618c07aa819e97363e5d
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-10-13 14:20:16 +0300

    updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import path from "node:path";
+import path from 'node:path';
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
@@ -14,16 +14,23 @@ export const SMTP = {
 
 export const APP_DOMAIN = process.env.APP_DOMAIN;
 
-export const TEMPLATE_DIR = path.resolve("src","templates");
-export const TEMP_UPLOAD_DIR = path.resolve("temp");
-export const UPLOAD_DIR = path.resolve( 'upload');
+export const TEMPLATE_DIR = path.resolve('src', 'templates');
+export const TEMP_UPLOAD_DIR = path.resolve('temp');
+export const UPLOAD_DIR = path.resolve('upload');
 
 export const BACKEND_HOST = process.env.BACKEND_HOST;
 
 export const CLOUDINARY = {
   NAME: process.env.CLOUDINARY_NAME,
   API_KEY: process.env.CLOUDINARY_API_KEY,
-  API_SECRET: process.env.CLOUDINARY_API_SECRET
+  API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const IS_CLOUDINARY_ENABLED = process.env.IS_CLOUDINARY_ENABLED;
+
+export const GOOGLE_CLIENT = {
+  ID: process.env.GOOGLE_CLIENT_ID,
+  SECRET: process.env.GOOGLE_CLIENT_SECRET,
+};
+
+export const SWAGGER_PATH = path.resolve('docs', 'swagger.json');
