07324178735e54b44d698e5a8270c82cc6f4e341
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-10-24 13:04:48 +0300

    corrected

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -13,6 +13,7 @@ import {
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {validateBody} from '../middlewares/validateBody.js';
+
 import { registerUserSchema, loginUserSchema, requestResetEmailSchema, resetPasswordSchema, googleOAuthSchema} from '../validation/auth.js';
 
 export const authRouter = Router();
